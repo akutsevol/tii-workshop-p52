c6ce79d5f9290ba8544b3e5ea27de4c7b98b8954
Aliaksei Kutsevol <Aliaksei.Kutsevol@tii.ae>
2024-06-10 02:16:54 +0400

    Sum 2

--- p52/src/lib.rs
+++ p52/src/lib.rs
@@ -1,5 +1,7 @@
 use std::thread;
-use std::sync::Arc;
+use std::sync::{Arc, mpsc};
+use std::sync::atomic::{AtomicU64, Ordering};
+use rayon::prelude::*;
 
 pub fn map_sum1<const N: usize>(data: Vec<u32>, map_fn: fn(u32) -> u64) -> u64 {
     let data_len = data.len();
@@ -31,6 +33,39 @@ pub fn map_sum1<const N: usize>(data: Vec<u32>, map_fn: fn(u32) -> u64) -> u64 {
     total_sum
 }
 
+pub fn map_sum2<const N: usize>(data: Vec<u32>, map_fn: fn(u32) -> u64) -> u64 {
+    let counter = AtomicU64::new(0);
+
+    data.into_par_iter()
+        .for_each(|num| {
+            counter.fetch_add(map_fn(num), Ordering::Relaxed);
+        });
+
+    counter.load(Ordering::Relaxed)
+}
+
+pub fn map_sum3<const N: usize>(data: Vec<u32>, map_fn: fn(u32) -> u64) -> u64 {
+    let (tx, rx) = mpsc::channel();
+    let chunk_size = data.len() / N;
+
+    for chunk in data.chunks(chunk_size) {
+        let chunk = chunk.to_vec(); // Clone the chunk to move into the closure
+        let tx_clone = tx.clone();
+        thread::spawn(move || {
+            let sum: u64 = chunk.iter().map(|&num| map_fn(num)).sum();
+            tx_clone.send(sum).unwrap();
+        });
+    }
+
+    drop(tx); // Drop the sender so the receiver can finish receiving
+
+    rx.iter().sum()
+}
+
+pub fn map_sum4<const N: usize>(data: Vec<u32>, map_fn: fn(u32) -> u64) -> u64 {
+    data.into_par_iter().map(map_fn).sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -43,6 +78,26 @@ mod tests {
     fn test_map_sum1() {
         let data = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         let result = map_sum1::<4>(data, example_map_fn);
+        assert_eq!(result, 110); // (1*2 + 2*2 + 3*2 + ... + 10*2) = 110
+    }
+    #[test]
+    fn test_map_sum2() {
+        let data = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let result = map_sum2::<4>(data.clone(), example_map_fn);
+        assert_eq!(result, 110); // (1*2 + 2*2 + 3*2 + ... + 10*2) = 110
+    }
+
+    #[test]
+    fn test_map_sum3() {
+        let data = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let result = map_sum3::<4>(data.clone(), example_map_fn);
+        assert_eq!(result, 110); // (1*2 + 2*2 + 3*2 + ... + 10*2) = 110
+    }
+
+    #[test]
+    fn test_map_sum4() {
+        let data = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let result = map_sum4::<4>(data.clone(), example_map_fn);
         assert_eq!(result, 110); // (1*2 + 2*2 + 3*2 + ... + 10*2) = 110
     }
 }
--- p52/src/main.rs
+++ p52/src/main.rs
@@ -1,10 +1,22 @@
 fn main () {
-    use p52::map_sum1;
+    use p52::{map_sum1, map_sum2, map_sum3, map_sum4};
 
     let data = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     let result = map_sum1::<4>(data, example_map_fn);
-    
-    println!("Result {:?}", result);
+
+    println!("Result [map_sum1] {:?}", result);
+
+    let data = vec![6, 7, 8, 9, 10, 6, 7, 8, 9, 10,];
+    let result = map_sum2::<4>(data.clone(), example_map_fn);
+    println!("Result [map_sum2] {:?}", result);
+
+    let data = vec![1, 32, 13, 4, 5, 6, 7, 8, 9, 10];
+    let result = map_sum3::<4>(data.clone(), example_map_fn);
+    println!("Result [map_sum3] {:?}", result);
+
+    let data = vec![1, 52, 3, 4, 25, 6, 47, 8, 9, 10];
+    let result = map_sum4::<4>(data.clone(), example_map_fn);
+    println!("Result [map_sum4] {:?}", result);
 }
 
 fn example_map_fn(x: u32) -> u64 {
