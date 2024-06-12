3212be6a4f4b8c18cb2f9312eeda8edd036b9d20
Aliaksei Kutsevol <Aliaksei.Kutsevol@tii.ae>
2024-06-12 16:18:39 +0400

    Added workflow

--- p52/src/lib.rs
+++ p52/src/lib.rs
@@ -1,34 +1,33 @@
-use std::thread;
 use std::sync::Arc;
+use std::thread;
 
+#[allow(dead_code)]
 fn map_sum1<F, const N: usize>(values: Vec<u32>, mapper: F) -> u64
 where
-    F: Fn(u32) -> u64 + Send + 'static,
+    F: Fn(u32) -> u64 + Send + 'static + Sync,
 {
-    let chunk_size = values.len() / N;
+    let chunk_size = (values.len() + N - 1) / N; // ensure chunks are evenly distributed
     let mut handles = vec![];
 
     let mapper = Arc::new(mapper); // Wrap the closure in an Arc
 
     for chunk in values.chunks(chunk_size) {
+        let chunk = chunk.to_vec(); // Clone the chunk
         let mapper = Arc::clone(&mapper); // Clone Arc to share ownership
-        let handle = thread::spawn(move || {
-            chunk.iter().map(|&v| mapper(v)).sum::<u64>()
-        });
+        let handle = thread::spawn(move || chunk.iter().map(|&v| mapper(v)).sum::<u64>());
         handles.push(handle);
     }
 
     handles.into_iter().map(|h| h.join().unwrap()).sum()
 }
 
-
-fn test() {
-    // let h1 = std::thread::spawn(move || {
-    //     // let a: u32  = buf.iter().sum();
-    //     let s: u32 = rc1.iter().sum();
-    //     std::thread::sleep(Duration::from_secs(1));
-    //     println!("Hi from thread {}!", s);
-    //     // buf[10];
-    //     2u32
-    // });
-}
+// fn test() {
+// let h1 = std::thread::spawn(move || {
+//     // let a: u32  = buf.iter().sum();
+//     let s: u32 = rc1.iter().sum();
+//     std::thread::sleep(Duration::from_secs(1));
+//     println!("Hi from thread {}!", s);
+//     // buf[10];
+//     2u32
+// });
+// }
--- p52/src/main.rs
+++ p52/src/main.rs
@@ -1,21 +1,19 @@
+use rayon::prelude::*;
 use std::arch::x86_64::*;
-use std::time::Duration;
-use std::fs::File;
+use std::collections::HashMap;
 use std::env;
+use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::path::Path;
-use std::collections::HashMap;
-use rayon::prelude::*;
 use std::sync::Arc;
+use std::time::Duration;
 use std::{cell::RefCell, sync::Mutex};
 
-mod lib;
-
 thread_local! {
-    static FOO: RefCell<u32> = RefCell::new(42);
+    static FOO: RefCell<u32> = const { RefCell::new(42) };
 }
 
-fn main () {
+fn main() {
     test_threads1();
     test_threads2();
     test_threads3();
@@ -26,12 +24,13 @@ fn main () {
     test_add_avx2();
 }
 
-fn computing_the_dot_product_of_two_large_vectors () {
+fn computing_the_dot_product_of_two_large_vectors() {
     println!("computing_the_dot_product_of_two_large_vectors:");
     let vec1 = vec![1, 2, 3, 4, 5];
     let vec2 = vec![6, 7, 8, 9, 10];
 
-    let dot_product: i32 = vec1.par_iter()
+    let dot_product: i32 = vec1
+        .par_iter()
         .zip(vec2.par_iter())
         .map(|(&x, &y)| x * y)
         .sum();
@@ -39,21 +38,28 @@ fn computing_the_dot_product_of_two_large_vectors () {
     println!("{}", dot_product); // Prints 130
 }
 
-fn processing_large_files () {
+fn processing_large_files() {
     println!("processing_large_files:");
     // Function to read a file into a Vec<String>
     fn read_lines<P>(filename: P) -> io::Result<Vec<String>>
-    where P: AsRef<Path>, {
+    where
+        P: AsRef<Path>,
+    {
         let file = File::open(filename)?;
         let reader = BufReader::new(file);
         reader.lines().collect()
     }
 
-    let path = env::current_dir().unwrap().into_os_string().into_string().unwrap();
+    let path = env::current_dir()
+        .unwrap()
+        .into_os_string()
+        .into_string()
+        .unwrap();
     let log_file_name = format!("{path}/src/{}", "large_log_file.log");
     let lines = read_lines(log_file_name).expect("Could not read file");
 
-    let error_count: usize = lines.par_iter()
+    let error_count: usize = lines
+        .par_iter()
         .filter(|line| line.contains("LQM-WIFI"))
         .count();
 
@@ -62,35 +68,32 @@ fn processing_large_files () {
 
 fn test_scope() {
     println!("test_scope");
-    fn factorial_sequential(n: u128) -> u128 { 
-        (1..=n).reduce(|multiple, next| multiple * next).unwrap() 
-    } 
+    fn factorial_sequential(n: u128) -> u128 {
+        (1..=n).reduce(|multiple, next| multiple * next).unwrap()
+    }
 
-    let mut map: Option<HashMap<String, usize>> = None; 
-    let mut factorial = 1; 
-    let mut other = None; 
-    rayon::scope(|s| { 
-        s.spawn(|_s| { 
-            let iter = 
-                (0..10000).enumerate().map(|(a, b)| (format!("index {}", a), b)); 
-            map = Some(HashMap::from_iter(iter)); 
-        }); 
-        s.spawn(|_s| { 
-            factorial = factorial_sequential(30); 
-        }); 
-        s.spawn(|_s| { 
-            other = Some("value") 
-        }) 
-    }); 
+    let mut map: Option<HashMap<String, usize>> = None;
+    let mut factorial = 1;
+    let mut other = None;
+    rayon::scope(|s| {
+        s.spawn(|_s| {
+            let iter = (0..10000)
+                .enumerate()
+                .map(|(a, b)| (format!("index {}", a), b));
+            map = Some(HashMap::from_iter(iter));
+        });
+        s.spawn(|_s| factorial = factorial_sequential(30));
+        s.spawn(|_s| other = Some("value"))
+    });
 
-    // println!("map {:?}", map.unwrap()); 
-    println!("factorial {:?}", factorial); 
-    // println!("other {:?}", other); 
-} 
+    // println!("map {:?}", map.unwrap());
+    println!("factorial {:?}", factorial);
+    // println!("other {:?}", other);
+}
 
 fn test_threads1() {
     println!("test_threads1:");
-    let buf = vec!(1u32, 2, 3);
+    let buf = vec![1u32, 2, 3];
     let rc1 = buf.clone();
     let rc2 = buf.clone();
 
@@ -143,7 +146,7 @@ fn test_threads2() {
 fn test_threads3() {
     println!("test_threads3:");
     let buf = Arc::new(Mutex::new(vec![1u32, 2, 3]));
-    
+
     let rc1 = buf.clone();
     let rc2 = buf.clone();
     let h1 = std::thread::spawn(move || {
@@ -177,7 +180,7 @@ fn test_add_sse2() {
     println!("test_add_sse2:");
     let a: [u32; 8] = [1, 2, 3, 4, 5, 6, 7, 8];
     let b: [u32; 8] = [8, 7, 6, 5, 4, 3, 2, 1];
-    let mut c: [u32; 8] = [0; 8]; // Initialize c with 
+    let mut c: [u32; 8] = [0; 8]; // Initialize c with
     unsafe {
         add_sse2(&a, &b, &mut c);
     }
@@ -188,7 +191,7 @@ fn test_add_avx2() {
     println!("test_add_avx2:");
     let a: [u32; 8] = [1, 2, 3, 4, 5, 6, 7, 8];
     let b: [u32; 8] = [8, 7, 6, 5, 4, 3, 2, 1];
-    let mut c: [u32; 8] = [0; 8]; // Initialize c with 
+    let mut c: [u32; 8] = [0; 8]; // Initialize c with
     unsafe {
         add_avx2(&a, &b, &mut c);
     }
